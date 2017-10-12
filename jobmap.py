0cab03c7132087f9f378988c3b889ac8a46bc56c
macbookpro2016 <1763132052@qq.com>
2017-10-12 22:48:18 +0800

    20171012
    可以爬取10页150条数据，存入数据库，
    cursor.execute(sql)不会返回结果，execute之后cursor.fetchone()会返回一条结果

--- jobmap.py
+++ jobmap.py
@@ -15,7 +15,8 @@ myheaders = {
 def down_loadpages(url):
 	data = requests.get(url,headers = myheaders).content
 	return data
-def parse_html(html):
+def parse_html(times,html,cursor):
+	times += 1
 	soup = BeautifulSoup(html,'html.parser')
 	job_div_soup = soup.find('div',attrs = {'class':'job-list'})
 	job_ul_soup = job_div_soup.find('ul')
@@ -23,11 +24,47 @@ def parse_html(html):
 	for job_li_soup in job_li_list:
 		job_name = job_li_soup.find('h3',attrs = {'class':'name'}).find('a').contents[0]
 		job_salary = job_li_soup.find('h3',attrs = {'class':'name'}).find('a').find('span',attrs = {'class':'red'}).getText()
-		print(job_name+':'+job_salary)
-
+		# print(job_name+':'+job_salary)
+		company_name = job_li_soup.find('div',attrs = {'class':'company-text'}).find('h3',attrs = {'class':'name'}).find('a').getText()
+		# print(company_name)
+		sql = 'insert into job_bj_py (description,salary,company) values("'+job_name+'","'+job_salary+'","'+company_name+'")'
+		# print(sql)
+		cursor.execute(sql)
+	next_page = 'http://www.zhipin.com'+soup.find('div',attrs = {'class':'page'}).find('a',attrs = {'class':'next'})['href']
+	# print(next_page)
+	if times < 10:
+		return times,next_page
+	return times,None
+# 创建表
+def create_table():
+	db = pymysql.connect('localhost','python','python','python3test',use_unicode = True,charset = 'utf8')
+	cursor = db.cursor()
+	cursor.execute('set names utf8')
+	cursor.execute('set character set utf8')
+	cursor.execute('set character_set_connection=utf8')
+	cursor.execute('drop table if exists job_bj_py')
+	sql = """create table job_bj_py(
+		rank int primary key auto_increment,
+		description varchar(100),
+		salary varchar(100),
+		company varchar(100)
+		)auto_increment = 1"""
+	cursor.execute(sql)
+	# cursor.execute('select  * from book_top_250')
+	# data = cursor.fetchone()
+	# print(data)
+	return db,cursor
 def main():
-	html = down_loadpages(boss_url)
-	parse_html(html)
+	# 实现自动翻页，向后翻9次一共10页150条数据
+	db,cursor = create_table()
+	url = boss_url
+	i = 0
+	while url:
+		html = down_loadpages(url)
+		i,url = parse_html(i,html,cursor)
+	db.commit()
+	db.close()
+
 
 if __name__ == '__main__':
 	main()
